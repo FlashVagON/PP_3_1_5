fc0b3101725311d1f5e5e5045d776f401c15b3b1
FlashVagON <djvagon@mail.ru>
2024-04-19 23:10:51 +0500

    rest made

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MainRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MainRestController.java
@@ -3,13 +3,13 @@ package ru.kata.spring.boot_security.demo.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -19,9 +19,30 @@ public class MainRestController {
     final UserService userService;
     final RoleService roleService;
 
-    @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+
+    @GetMapping("/userlist")
+    public List<User> getUsersList(){
+        return userService.listUsers();
+    }
+    @GetMapping("/rolelist")
+    public List<Role> getRolesList(){
+        return roleService.getAllRoles();
+    }
+    @PostMapping("/adduser")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         userService.addUser(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("/edituser")
+    public ResponseEntity<User> editUser(@RequestBody User user) {
+        userService.updateUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/deleteuser")
+    public ResponseEntity<String> deleteUser(@RequestBody Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>("User deleted", HttpStatus.OK);
     }
 }
